c8e2f14bd5eefdd9cbab3b4e2387f1eebd345cca
mudassirudd <mudassirudd@gmail.com>
2025-09-27 00:16:58 +0530

    day10 project

---
+++ day10-dad-jokes/script.js
@@ -0,0 +1,16 @@
+const jokeDiv = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', makeJoke)
+makeJoke()
+async function makeJoke() {
+  const res = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+
+  const data = await res.json()
+
+  jokeDiv.innerHTML = data.joke
+}
